f8cccc7390142f45167d68b74c51bf42be8c8dc4
msidorenkova <sidorenkova_ma@mail.ru>
2020-07-16 17:47:39 +0300

    Updated Homework Sockets

--- Homework_17 (Sockets)/tcp_server_max.py
+++ Homework_17 (Sockets)/tcp_server_max.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import logging
 import socket
 
@@ -24,31 +23,4 @@ while True:
         logging.info(message)
 
     print('Disconnected:', addr)
-=======
-import socket
-import logging
-
-logging.basicConfig(
-                    filename='mouse.log',
-                    level=logging.INFO,
-                    format='%(asctime)s: %(message)s'
-                    )
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-sock.bind(('localhost', 9999))
-sock.listen(5)
-while True:
-    client, addr = sock.accept()
-    print('Connected:', addr)
-
-    while True:
-        data = client.recv(1024)
-        if not data:
-            break
-        message = data.decode('utf-8')
-        print(f'Received data from: {addr}')
-        logging.info(message)
-
->>>>>>> 4851d4dcb28b30f15c91fe512a44d6b0acb31dbe
     client.close()
