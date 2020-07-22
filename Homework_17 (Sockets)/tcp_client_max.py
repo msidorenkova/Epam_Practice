816a7dbdb5464546d9da6f55ca43a53ce2960b3c
msidorenkova <sidorenkova_ma@mail.ru>
2020-07-22 06:18:48 +0300

    Updated HW Sockets

--- Homework_17 (Sockets)/tcp_client_max.py
+++ Homework_17 (Sockets)/tcp_client_max.py
@@ -1,9 +1,9 @@
 import socket
+import sys
 import threading
 import time
 from datetime import datetime
 
-import keyboard
 import pyWinhook
 import pythoncom
 
@@ -14,16 +14,36 @@ class Collector:
         self.start = int(time.time())
         self.counter = 0
         self.flag = False
+        self.hm = pyWinhook.HookManager()
 
     def start_collect(self):
         self.flag = True
+        self.hm.MouseAll = self._timer
+        self.hm.HookMouse()
+        self.hm.KeyDown = self._onKeyboardEvent
+        self.hm.HookKeyboard()
+        pythoncom.PumpMessages()
 
-    def timer(self):
+    def _timer(self, event):
         if self.flag:
             current_time = int(time.time())
             if self.start != current_time:
                 self.counter += 1
                 self.start = current_time
+        return True
+
+    def _onKeyboardEvent(self, event):
+        if event.Key == 'S' and self.flag:
+            self.stop_collect()
+            self.cleanup()
+            print('Stopping data collection')
+        elif event.Key == 'R' and not self.flag:
+            print('Resuming data collection')
+            self.start_collect()
+        elif event.Key == 'Q':
+            print('Stopping execution')
+            sys.exit()
+        return True
 
     def get_current_state(self):
         return self.counter
@@ -36,56 +56,27 @@ class Collector:
         self.flag = False
 
 
-def create_thread(func):
-    thread = threading.Thread(target=func)
-    thread.daemon = True
-    thread.start()
-
-
-def start_client():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(('localhost', 9999))
-
-    while True:
-        time.sleep(60)
-        if collector.flag:
-            data = f'Mouse events, seconds: {str(collector.get_current_state())}'
-            collector.cleanup()
-            sock.send(data.encode('utf-8'))
-            print(f'{datetime.now()}: Data has been sent')
-        else:
-            data = f'Data collection disabled'
-            sock.send(data.encode('utf-8'))
-            print(f'{datetime.now()}: Data has been sent')
-    sock.close()
-
-
-def detect_mouse_event():
-    hm = pyWinhook.HookManager()
-    hm.MouseAll = onMouseEvent
-    hm.HookMouse()
-    pythoncom.PumpMessages()
-
-
-def onMouseEvent(event):
-    collector.timer()
-    return True
-
-
 collector = Collector()
-create_thread(detect_mouse_event)
-create_thread(start_client)
-collector.start_collect()
+thread = threading.Thread(target=collector.start_collect)
+thread.daemon = True
+thread.start()
+
 print('Starting data collection')
 print('Press "s" to stop collecting data, "r" to resume collecting data or "q" to close a client: \n')
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('localhost', 9999))
+
 while True:
-    if keyboard.is_pressed('s') and collector.flag:
-        collector.stop_collect()
-        print('Stopping data collection')
-    elif keyboard.is_pressed('r') and not collector.flag:
-        collector.start_collect()
-        print('Resuming data collection')
-    elif keyboard.is_pressed('q'):
-        print('Stopping execution')
-        break
+    time.sleep(60)
+
+    if collector.flag:
+        data = f'Mouse events, seconds: {str(collector.get_current_state())}'
+        collector.cleanup()
+    else:
+        data = 'Data collection disabled'
+
+    sock.send(data.encode('utf-8'))
+    print(f'{datetime.now()}: Data has been sent')
+
+sock.close()
